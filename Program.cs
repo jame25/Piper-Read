19298f67c4f4a0b6c7bb43d829855a6fd1974c2e
jame25 <jame25@users.noreply.github.com>
2024-04-09 19:33:05 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,32 +1,16 @@
-using System;
-using System.Windows.Forms;
+namespace piper_read;
 
-namespace PiperTTS
+static class Program
 {
-    static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        static Mutex mutex = null;
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            const string appName = "piper_read";
-            bool createdNew;
-
-            mutex = new Mutex(true, appName, out createdNew);
-
-            if (!createdNew)
-            {
-                MessageBox.Show("Another instance of Piper Read is already running.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                return;
-            }
-
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }    
 }
