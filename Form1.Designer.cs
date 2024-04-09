c4b873481d9235b05c9791841e31f8c21484f88a
jame25 <jame25@users.noreply.github.com>
2024-04-09 19:34:42 +0100

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,148 +1,122 @@
-namespace PiperTTS
+namespace piper_read;
+
+partial class Form1
 {
-    partial class Form1
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.TrackBar speedTrackBar;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        if (disposing && (components != null))
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            components.Dispose();
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.convertButton = new System.Windows.Forms.Button();
-            this.clearButton = new System.Windows.Forms.Button();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.menuStrip1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
-            this.textBox1.Location = new System.Drawing.Point(12, 38);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(720, 274);
-            this.textBox1.TabIndex = 0;
-            // 
-            // convertButton
-            // 
-            this.convertButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
-            this.convertButton.Location = new System.Drawing.Point(12, 320);
-            this.convertButton.Name = "convertButton";
-            this.convertButton.Size = new System.Drawing.Size(75, 23);
-            this.convertButton.TabIndex = 1;
-            this.convertButton.Text = "Convert";
-            this.convertButton.UseVisualStyleBackColor = true;
-            this.convertButton.Click += new System.EventHandler(this.ConvertButton_Click);
-            // 
-            // clearButton
-            // 
-            this.clearButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
-            this.clearButton.Location = new System.Drawing.Point(663, 320);
-            this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(75, 23);
-            this.clearButton.TabIndex = 2;
-            this.clearButton.Text = "Clear";
-            this.clearButton.UseVisualStyleBackColor = true;
-            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem,
-                        this.aboutToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(750, 24);
-            this.menuStrip1.TabIndex = 3;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.exitToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
-            this.fileToolStripMenuItem.Text = "File";
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
-            // 
-            // aboutToolStripMenuItem
-            // 
-            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
-            this.aboutToolStripMenuItem.Text = "About";
-            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(750, 350);
-            this.Controls.Add(this.clearButton);
-            this.Controls.Add(this.convertButton);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.menuStrip1);
-            this.MainMenuStrip = this.menuStrip1;
-            this.Name = "Form1";
-            this.Text = "Piper Read";
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-            this.speedTrackBar = new System.Windows.Forms.TrackBar();
-            this.speedTrackBar.Location = new System.Drawing.Point(500, 600); // Adjust the position as needed
-            this.speedTrackBar.Name = "speedTrackBar";
-            this.speedTrackBar.Size = new System.Drawing.Size(200, 45); // Adjust the size as needed
-            this.speedTrackBar.Minimum = 1;
-            this.speedTrackBar.Maximum = 9;
-            this.speedTrackBar.SmallChange = 1;
-            this.speedTrackBar.LargeChange = 1;
-            this.speedTrackBar.TickFrequency = 1;
-            this.speedTrackBar.Value = 5;
-            this.speedTrackBar.MouseUp += new System.Windows.Forms.MouseEventHandler(this.speedTrackBar_MouseUp);
-            this.Controls.Add(this.speedTrackBar);
-        }
-
-        #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button convertButton;
-        private System.Windows.Forms.Button clearButton;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        base.Dispose(disposing);
     }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+
+        // Create a MenuStrip control
+        MenuStrip menuStrip = new MenuStrip();
+        this.Controls.Add(menuStrip);
+
+        // Create a "File" menu item
+        ToolStripMenuItem fileMenu = new ToolStripMenuItem("File");
+        menuStrip.Items.Add(fileMenu);
+
+        // Create an "Open" menu item
+        ToolStripMenuItem openMenuItem = new ToolStripMenuItem("Open");
+        openMenuItem.Click += new EventHandler(OpenMenuItem_Click);
+        fileMenu.DropDownItems.Add(openMenuItem);
+
+        // Create an "Exit" menu item
+        ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Exit");
+        exitMenuItem.Click += new EventHandler(ExitMenuItem_Click);
+        fileMenu.DropDownItems.Add(exitMenuItem);
+
+        // Create an "About" menu item
+        ToolStripMenuItem aboutMenuItem = new ToolStripMenuItem("About");
+        aboutMenuItem.Click += new EventHandler(AboutMenuItem_Click);
+        menuStrip.Items.Add(aboutMenuItem);
+
+        this.txtInput = new System.Windows.Forms.TextBox();
+        this.btnConvert = new System.Windows.Forms.Button();
+        this.btnClearStop = new System.Windows.Forms.Button();
+        this.lblModelName = new System.Windows.Forms.Label();
+        this.SuspendLayout();
+
+        // txtInput
+        this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+        this.txtInput.Location = new System.Drawing.Point(0, 24);
+        this.txtInput.Multiline = true;
+        this.txtInput.Name = "txtInput";
+        this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+        this.txtInput.Size = new System.Drawing.Size(800, 397);
+        this.txtInput.TabIndex = 0;
+
+        // btnConvert
+        this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+        this.btnConvert.Location = new System.Drawing.Point(12, 415);
+        this.btnConvert.Name = "btnConvert";
+        this.btnConvert.Size = new System.Drawing.Size(75, 23);
+        this.btnConvert.TabIndex = 1;
+        this.btnConvert.Text = "Convert";
+        this.btnConvert.UseVisualStyleBackColor = true;
+        this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+
+        // btnClearStop
+        this.btnClearStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+        this.btnClearStop.Location = new System.Drawing.Point(704, 415);
+        this.btnClearStop.Name = "btnClearStop";
+        this.btnClearStop.Size = new System.Drawing.Size(75, 23);
+        this.btnClearStop.TabIndex = 2;
+        this.btnClearStop.Text = "Clear";
+        this.btnClearStop.UseVisualStyleBackColor = true;
+        this.btnClearStop.Click += new System.EventHandler(this.btnClearStop_Click);
+
+        // lblModelName
+        this.lblModelName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+        this.lblModelName.AutoSize = true;
+        this.lblModelName.Location = new System.Drawing.Point(93, 415);
+        this.lblModelName.Name = "lblModelName";
+        this.lblModelName.Size = new System.Drawing.Size(38, 15);
+        this.lblModelName.TabIndex = 3;
+        this.lblModelName.Text = "Model";
+
+        // Form1
+        this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Controls.Add(this.lblModelName);
+        this.Controls.Add(this.btnClearStop);
+        this.Controls.Add(this.btnConvert);
+        this.Controls.Add(this.txtInput);
+        this.MainMenuStrip = menuStrip;
+        this.Name = "Form1";
+        this.Text = "Piper Read";
+        this.ResumeLayout(false);
+        this.PerformLayout();
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox txtInput;
+    private System.Windows.Forms.Button btnConvert;
+    private System.Windows.Forms.Button btnClearStop;
+    private System.Windows.Forms.Label lblModelName;
 }
